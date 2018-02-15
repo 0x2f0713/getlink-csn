79bb8522aaeede0a7a2b2de95e93e45ddcbcbca3
0x2f0713 <34441920+0x2f0713@users.noreply.github.com>
2018-02-15 21:15:54 +0700

    Update Get_file.php

--- Get_file.php
+++ Get_file.php
@@ -1,12 +1,10 @@
     <?php
 include './simple_html_dom.php';
-// Create DOM from URL or file
+
 $url = $_GET[url];
+
 function get_file($url) {
 $doc = file_get_html(str_replace('.html', '_download.html', $url));
-// Find all links 
-#foreach($html->find('a') as $element) 
-       #$data = $element->href . '<br>';
 foreach ($doc->find('div#downloadlink2') as $e) {
     foreach ($e->find('b') as $f) {
         foreach ($f->find('a') as $g) {
@@ -19,28 +17,13 @@ foreach ($doc->find('div#downloadlink2') as $e) {
 return $data;
 }
 
+// Có thể sử dụng tùy bạn
 function get_name($link){;
 $html = file_get_html($link);
 $filename = preg_replace('/~"#%&*:<>?\{|}. /', ' ', $html->find('h1.viewtitle',0)->plaintext);
-#filename = $html->find('h1.viewtitle',0)->plaintext;
-  return urlencode($filename);
+  return $filename;
 }
 
-$link = get_file($url)['128kbps'];
-$title = get_name($url).'.mp3';
-#$id = file_get_contents('http://'. $_SERVER['SERVER_NAME'] .'/post9/upload.php' . '?title=' . $title . '&url=' . $link);
-$id = file_get_contents('https://www.namhaiit.ml/php//dropbox.php?url='.$link.'&filename='.$title);
+$link = get_file($url);
 
-      $rep  = array(
-          'messages' => array(
-              0 => array(
-                  'attachment' => array(
-                      'type' => 'audio',
-                      'payload' => array(
-                          'url' => $id
-                      )
-                  )
-              ),
-          )
-      );
-echo json_encode($rep);
+echo json_encode($link);
